835d68cb53a46ee265f526036c8f3c9b0d6ea16c
uehara1414 <b1015062@fun.ac.jp>
2016-12-02 09:12:19 +0900

    Update crawl_unit_test.py

--- tests/crawl_unit_test.py
+++ tests/crawl_unit_test.py
@@ -11,34 +11,39 @@ class CrawlUnitTest(unittest.TestCase):
         CrawlUnit("http://www.google.co.jp/", '/', 0)
 
     def test_accessible(self):
-        crawl_unit = CrawlUnit("http://www.google.co.jp/", '/', 0)
+        crawl_unit = CrawlUnit("http://www.google.co.jp/", "http://www.google.co.jp/", 0)
         crawl_unit.crawl
         crawl_unit.get_url_if_valid
         crawl_unit.get_link_set
 
     def test_crawl_valid_html_url(self):
-        crawl_unit = CrawlUnit("http://www.google.co.jp/", '/', 0)
+        crawl_unit = CrawlUnit("http://www.google.co.jp/", "http://www.google.co.jp/", 0)
+        crawl_unit.crawl()
         self.assertEqual(crawl_unit.get_url_if_valid(), "http://www.google.co.jp")
         self.assertTrue(len(crawl_unit.get_link_set()) > 0)
 
     def test_crawl_an_invalid_url(self):
-        crawl_unit = CrawlUnit('http://www.google/', '/', 0)
+        crawl_unit = CrawlUnit('http://www.google/', 'http://www.google/', 0)
+        crawl_unit.crawl()
+        self.assertTrue('text/html' in crawl_unit.content_type)
         self.assertEqual(crawl_unit.get_url_if_valid(), '')
         self.assertTrue(crawl_unit.get_link_set() == set())
 
     def test_crawl_valid_image_url(self):
-        crawl_unit = CrawlUnit("https://avatars3.githubusercontent.com/u/15319686", '/', 0)
+        crawl_unit = CrawlUnit("https://avatars3.githubusercontent.com/u/15319686", "https://avatars3.githubusercontent.com/u/15319686", 0)
+        crawl_unit.crawl()
+        self.assertTrue('image/png', crawl_unit.content_type)
         self.assertEqual(crawl_unit.get_url_if_valid(), "https://avatars3.githubusercontent.com/u/15319686")
         self.assertEqual(crawl_unit.get_link_set(), set())
 
     def test_get_href_links(self):
-        crawl_unit = CrawlUnit("http://www.google.co.jp/", '/', 0)
+        crawl_unit = CrawlUnit("http://www.google.co.jp/", "http://www.google.co.jp/", 0)
         text = "<a href='index.html'></a>"
         soup = BeautifulSoup(text)
         self.assertEqual(crawl_unit._get_href_links(soup), {"http://www.google.co.jp/index.html"})
 
     def test_get_image_srcs(self):
-        crawl_unit = CrawlUnit("http://www.google.co.jp/", '/', 0)
+        crawl_unit = CrawlUnit("http://www.google.co.jp/", "http://www.google.co.jp/", 0)
         text = "<img src='/foo.png'>"
         soup = BeautifulSoup(text)
         print(crawl_unit._get_image_srcs(soup))
