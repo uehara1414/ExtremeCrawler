ad9dd6a8e1735f97e951d2238a99e0c494f3ec76
uehara1414 <b1015062@fun.ac.jp>
2016-12-02 12:52:05 +0900

    Temporary implementation

--- extremecrawler/extreme_crawler.py
+++ extremecrawler/extreme_crawler.py
@@ -8,31 +8,45 @@ from .crawl_unit import CrawlUnit
 
 
 class ExtremeCrawler:
-    """The information about the one crawl
+    """クローリング全体を司るクラスです。
 
-    url: The url which will be crawled
-    origin_url: The url which The url is linked from.
-    depth: now depth.
+    コンストラクタで初期設定を行い、crawl() で実際のクローリングを行います。結果はcrawl()
+    自体がジェネレータとなっているので、検証された結果が逐次返されるほか、
+    crawl()終了後に get_links(content_filter=["imgage", "html", "css"]) といった形で
+
     """
 
-    def __init__(self, domain:str, index:str='/', max_depth:int=1024):
+    def __init__(self, domain: str, index: str='/', max_depth: int=1024):
+        """
+
+        :param domain: 探索を行うドメイン名。このドメイン以下のコンテンツのみを収集します。
+        :param index: 探索を行う開始地点のパス
+        :param max_depth: 探索を行う最大の深さ
+        """
         self.domain = domain
         self.max_depth = max_depth
 
         self.crawl_queue = PriorityQueue()
         self.crawled_url_set = set()
+        self.none_html_url_set = set()
 
         if not index.startswith('http'):
             self.index = urljoin(domain, index)
         else:
             self.index = index
 
-    def crawl(self):
+    def crawl(self, content_filter=None):
+
+        if content_filter is None:
+            content_filter = ['text/html']
+        elif not isinstance(content_filter, str):
+            content_filter = [content_filter]
+
         self.crawl_queue.put(CrawlUnit(self.domain, self.index, 0))
         while not self.crawl_queue.empty():
             crawl_unit = self.crawl_queue.get_nowait()
 
-            if crawl_unit.url in self.crawled_url_set:
+            if self._is_already_crawled(crawl_unit.url):
                 continue
 
             if crawl_unit.depth == self.max_depth:
@@ -40,22 +54,24 @@ class ExtremeCrawler:
                 yield crawl_unit.url
                 continue
 
-            links = []
             try:
-                links = crawl_unit.get_href_links()
-            except NotHtmlError:
-                continue
-            except requests.ConnectionError:
-                print('Connection error has occured. Retry to access {} later.'.format(crawl_unit.url))
-                self.crawl_queue.put(CrawlUnit(self.domain, crawl_unit.url, crawl_unit.depth))
-            except TimeoutError:
-                print('Timeout error has occured. Retry to access {} later.'.format(crawl_unit.url))
+                crawl_unit.crawl()
+            except (requests.ConnectionError, TimeoutError):
+                print('Connection error has occurred. Retry to access {} later.'.format(crawl_unit.url))
                 self.crawl_queue.put(CrawlUnit(self.domain, crawl_unit.url, crawl_unit.depth))
 
             self.crawled_url_set.add(crawl_unit.url)
 
-            for url in links:
-                if url not in self.crawled_url_set:
-                    self.crawl_queue.put(CrawlUnit(self.domain, url, crawl_unit.depth+1))
+            for url in self._filter_crawled_urls(crawl_unit.get_link_set()):
+                self.crawl_queue.put(CrawlUnit(self.domain, url, crawl_unit.depth+1))
 
-            yield crawl_unit.url
+            for content_type in crawl_unit.content_type:
+                if content_type in content_filter:
+                    if crawl_unit.get_url_if_valid():
+                        yield crawl_unit.get_url_if_valid()
+
+    def _is_already_crawled(self, url):
+        return url in self.crawled_url_set
+
+    def _filter_crawled_urls(self, url_set: set):
+        return filter(lambda x: x in self.crawled_url_set, url_set)
