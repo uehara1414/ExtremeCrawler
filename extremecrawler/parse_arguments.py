a17dbe907d91f6f53bfea973326963587a98ea88
uehara1414 <b1015062@fun.ac.jp>
2016-12-06 13:37:31 +0900

    Add --image-only option

--- extremecrawler/parse_arguments.py
+++ extremecrawler/parse_arguments.py
@@ -9,6 +9,8 @@ def parse_arguments(argv: list):
                         help='The path which the search starts. example: /index.html defalut: /', default='/')
     parser.add_argument('--depth',
                         help='search depth', default=1024, type=int)
+    parser.add_argument('--image-only',
+                        help='Get image\'s url only.', action='store_true', default=False)
 
     args = parser.parse_args(args=argv)
 
--- tests/argparse_test.py
+++ tests/argparse_test.py
@@ -25,3 +25,14 @@ class ArgparseTest(unittest.TestCase):
         self.assertEqual(parsed.domain, 'http://www.google.co.jp')
         self.assertEqual(parsed.depth, 1024)
         self.assertEqual(parsed.index, 'index.html')
+
+    def test_parse_image_filter(self):
+        arguments = 'http://www.google.co.jp --image-only'.split(' ')
+        parsed = parse_arguments(arguments)
+        self.assertEqual(parsed.image_only, True)
+
+    def test_parse_image_filter2(self):
+        arguments = 'http://www.google.co.jp'.split(' ')
+        parsed = parse_arguments(arguments)
+        self.assertEqual(parsed.image_only, False)
+
